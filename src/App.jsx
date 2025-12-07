3c3ff892a86f08f0c5503893b558850a26bd51c1
Abdullohabdumalikov <a3766668@gmail.com>
2025-12-07 11:34:19 +0500

    winn

--- src/App.jsx
+++ src/App.jsx
@@ -6,45 +6,22 @@ import About from './pages/About/About'
 import Regist from './pages/Regist/Regist'
 import Shop from './pages/Shop/Shop'
 import Support from './pages/Support/Support'
+import Artice from './pages/Article/Artice'
 
 import './App.css'
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Register />} />
-        <Route
-          path="/"
-          element={
-            <ProtectedRoute>
-              <LayOut>
-                <Home />
-              </LayOut>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/about"
-          element={
-            <ProtectedRoute>
-              <LayOut>
-                <About />
-              </LayOut>
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="*"
-          element={
-            <ProtectedRoute>
-              <LayOut>
-                <NotFound />
-              </LayOut>
-            </ProtectedRoute>
-          }
-        />
-      </Routes>
+      <p>Lorem </p>
+      <Header />
+      <Footer />
+      <Home />
+      <About />
+      <Regist />
+      <Shop />
+      <Support />
+      <Artice />
     </>
   )
 }
---
+++ src/pages/Article/Artice.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Artice() {
+  return (
+    <div>
+      <p>A</p>
+    </div>
+  )
+}
+
+export default Artice
